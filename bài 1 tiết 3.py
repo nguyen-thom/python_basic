38de911e46a78f05a7b16045590dd4970d18ede1
datn39469@gmail.com <datn39469@gmail.com>
2023-12-30 18:11:09 +0700

    no message

---
+++ bài 1 tiết 3.py
@@ -0,0 +1,16 @@
+n=0
+a=float(input("ký điện="))
+
+if   1  > a < 50 :
+    n=a*1.678
+elif  51  >  a < 100 :
+    n=a*1.734
+elif   101 >   a  < 200 :
+    n=a*2.014
+elif  201 >a<300 :
+    n=a*2.536
+elif 301 >a<400:
+    n=a*2.834
+elif a >401 :
+    n=a*2.927
+else :print (n)
---
+++ tiet 2 bai tap 2.py
@@ -0,0 +1,6 @@
+#bai_tap2:tien 2
+a = 4
+for i in range (1,a+1):
+        for j in range (1,i+1):
+            print("2",end="")
+        print()
